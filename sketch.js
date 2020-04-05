d939c58b336beddf65b694de86f9e886a204cb38
Veni13 <veni7678@gmail.com>
2020-04-05 21:49:17 +0530

    changed the height of the ground

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ function setup(){
 
     box1 = new Box(200,300,50,50);
     box2 = new Box (240,100,50,100);
-    ground = new Ground(200,390,400,20);git 
+    ground = new Ground(200,390,400,60);git 
 }
 
 function draw(){
